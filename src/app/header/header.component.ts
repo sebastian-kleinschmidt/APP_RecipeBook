a1662bd53c54c95d488d870bf7ebd31b13aa9fe7
Sebastian Kleinschmidt <sebastiankleinschmidt@hotmail.com>
2019-09-05 10:17:05 +0200

    feat: adds navigation eventbinding to header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
 })
 
 export class HeaderComponent {
+@Output() featureSelector: EventEmitter<string> = new EventEmitter();
+onSelect(feature: string) {
+  this.featureSelector.emit(feature);
+}
 
 }
